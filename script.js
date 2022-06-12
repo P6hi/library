3099c03516d86a875a4131451d53138910deb9a8
Arthur <a-murk@mail.ee>
2022-06-12 14:24:15 +0300

    Add function to confirm button
    Clicking the confirm button when the values are not empty will add the data into an array

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const form = document.querySelector('form');
 const formBtn = document.querySelector('#formBtn');
 const confirmBtn = document.querySelector('button[type="submit"]');
+const titleField = document.querySelector('#title');
+const authorField = document.querySelector('#author');
+const pageField = document.querySelector('#pages');
+const hasReadField = document.querySelector('#hasRead');
 
 formBtn.addEventListener('click', () => {
     form.classList.toggle('show');
+})
+
+confirmBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (titleField.value !== '' && authorField.value !== '' && pageField.value !== '') {
+        addBookToLibrary();
+    }
 })
 
 let myLibrary = [];
@@ -15,4 +26,7 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary();
+function addBookToLibrary() {
+    const newBook = new Book(titleField.value, authorField.value, pageField.value, hasReadField.checked);
+    myLibrary.push(newBook);
+};
