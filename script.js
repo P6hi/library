444f3d8fdc86250b13699fb6791b3d2c5dcaee34
Arthur <a-murk@mail.ee>
2022-06-12 14:09:16 +0300

    Create basic skeleton functions

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const formBtn = document.querySelector('#formBtn');
 formBtn.addEventListener('click', () => {
     form.classList.toggle('show');
 })
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary();
